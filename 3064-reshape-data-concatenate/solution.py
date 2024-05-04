00ca0b338a08e0deec974c5ba12fb379064755c9
Thissya <125356637+thissya@users.noreply.github.com>
2024-05-04 07:11:02 +0000

    [LeetCode Sync] Runtime - 601 ms (10.81%), Memory - 65.3 MB (98.25%)

---
+++ 3064-reshape-data-concatenate/solution.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def concatenateTables(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
+    df1=pd.DataFrame(df1)
+    df2=pd.DataFrame(df2)
+    df=pd.concat([df1,df2])
+    return df
