d76301dc13513033de436cee2665a0df2d140645
Thissya <125356637+thissya@users.noreply.github.com>
2024-05-25 10:40:26 +0000

    [LeetCode Sync] Runtime - 1 ms (45.53%), Memory - 6.4 MB (100.00%)

---
+++ 0345-reverse-vowels-of-a-string/solution.c
@@ -0,0 +1,22 @@
+char* reverseVowels(char* s) {
+    
+    int n=strlen(s);
+    char a[n];
+    int c=0;
+    for(int i=n-1;i>=0;i--)
+    {
+        if(s[i]=='a' || s[i]=='e' ||s[i]=='i' ||s[i]=='o'||s[i]=='u'|| s[i]=='A' || s[i]=='E' ||s[i]=='I' ||s[i]=='O'||s[i]=='U' )
+        {
+            a[c++]=s[i];   
+        }
+    }
+    c=0;
+    for(int i=0;i<n;i++)
+    {
+        if(s[i]=='a' || s[i]=='e' ||s[i]=='i' ||s[i]=='o'||s[i]=='u'|| s[i]=='A' || s[i]=='E' ||s[i]=='I' ||s[i]=='O'||s[i]=='U' )
+        {
+            s[i]=a[c++];
+        }
+    }
+    return s;
+}
