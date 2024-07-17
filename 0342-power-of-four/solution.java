46f48045dd422f18d617244a553adab864751863
Thissya <125356637+thissya@users.noreply.github.com>
2024-07-17 12:07:20 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.8 MB (67.70%)

---
+++ 0342-power-of-four/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPowerOfFour(int n) 
+    {
+        if(n==1)
+        return true;
+        if(n<=0)return false;
+        while(n>1)
+        {
+            if(n%4!=0)
+                return false;
+            n/=4;
+        }    
+        return true;
+    }
+}
