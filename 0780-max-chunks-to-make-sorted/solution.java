bfa117c3472025e8707df636dc697e4f38c4f04a
Thissya <125356637+thissya@users.noreply.github.com>
2024-07-24 17:56:01 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.8 MB (71.64%)

---
+++ 0780-max-chunks-to-make-sorted/solution.java
@@ -0,0 +1,14 @@
+class Solution 
+{
+    public int maxChunksToSorted(int[] arr) 
+    {
+        int max=0,c=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            max=Math.max(max,arr[i]);
+            if(i==max)
+                c++;
+        }        
+        return c;
+    }
+}
