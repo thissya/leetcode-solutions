b61846255d1fdc352f155a0255f5da06fe618334
Thissya <125356637+thissya@users.noreply.github.com>
2024-07-25 13:17:39 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.1 MB (93.55%)

---
+++ 0062-unique-paths/solution.java
@@ -0,0 +1,25 @@
+class Solution 
+{
+    public int uniquePaths(int m, int n) 
+    {
+        int[][] mat= new int[m][n];
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i==0 || j==0)
+                {
+                    mat[i][j]=1;
+                }
+            }
+        }        
+        for(int i=1;i<m;i++)
+        {
+            for(int j=1;j<n;j++)
+            {
+                mat[i][j]=mat[i-1][j]+mat[i][j-1];
+            }
+        }
+        return mat[m-1][n-1];    
+    }
+}
