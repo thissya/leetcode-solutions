baaa47509353ff3f0b4badc81d596d48931e5987
Thissya <125356637+thissya@users.noreply.github.com>
2024-08-01 17:19:02 +0000

    [LeetCode Sync] Runtime - 2 ms (62.37%), Memory - 41.9 MB (50.59%)

---
+++ 0389-find-the-difference/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int ans=0;
+        for(int i=0;i<t.length();i++)
+        {
+            ans+=t.charAt(i);
+        }
+        for(int i=0;i<s.length();i++)
+        {
+            ans-=s.charAt(i);
+        }
+        return (char)ans;
+    }
+}
