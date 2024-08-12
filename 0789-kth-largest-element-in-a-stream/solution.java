86753a9146d5bd8cdde1df4b508422018d8be532
Thissya <125356637+thissya@users.noreply.github.com>
2024-08-12 18:16:52 +0000

    [LeetCode Sync] Runtime - 14 ms (100.00%), Memory - 46.9 MB (100.00%)

---
+++ 0789-kth-largest-element-in-a-stream/solution.java
@@ -0,0 +1,27 @@
+class KthLargest {
+    private final int  k;
+    private final PriorityQueue<Integer> minheap;
+    public KthLargest(int k, int[] nums) {
+        this.k=k;
+        minheap= new PriorityQueue<>();
+        for(int num :nums)
+        {
+            add(num);
+        }
+    }
+
+    public int add(int val) {
+        minheap.add(val);
+        if(minheap.size()>k)
+        {
+            minheap.poll();
+        }
+        return minheap.peek();
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
