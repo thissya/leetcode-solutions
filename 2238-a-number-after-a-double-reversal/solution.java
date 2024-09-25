273c0e6655e2b34a641bfe383e961a6624b4f9d6
Thissya <125356637+thissya@users.noreply.github.com>
2024-09-25 15:04:24 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.4 MB (66.80%)

--- 2238-a-number-after-a-double-reversal/solution.java
+++ 2238-a-number-after-a-double-reversal/solution.java
@@ -1,7 +1,18 @@
 class Solution {
     public boolean isSameAfterReversals(int n) {
-        if(n==0)return true;
-        if(n%10==0)return false;
-        return true;
+        int temp =n;
+        int rev=0;
+        while(temp>0)
+        {
+            rev=(rev*10)+ temp%10;
+            temp/=10;
+        }
+        int temp2=0;
+        while(rev>0)
+        {
+            temp2=(temp2*10)+rev%10;
+            rev/=10;
+        }
+        return (temp2==n);
     }
 }
