36404f524ee23d90547c9b9a2f9304890b9e7b1b
Thissya <125356637+thissya@users.noreply.github.com>
2024-10-01 18:40:57 +0000

    [LeetCode Sync] Runtime - 1 ms (87.03%), Memory - 40.8 MB (73.01%)

--- 0069-sqrtx/solution.java
+++ 0069-sqrtx/solution.java
@@ -2,6 +2,20 @@ class Solution
 {
     public int mySqrt(int x) 
     {
-        return (int)Math.sqrt(x);
+        int l=1,r=x;
+        int mid;
+        while(l<=r)
+        {
+            mid=l+(r-l)/2;
+            if((long)mid*mid > (long)x)
+            {
+                r=mid-1;
+            }
+            else if(mid*mid ==x)return mid;
+            else{
+                l=mid+1;
+            } 
+        }
+        return Math.round(r);
     }
 }
