5c842cf48520618536be2b633a21ccf80f821ca3
Thissya <125356637+thissya@users.noreply.github.com>
2024-10-31 18:42:11 +0000

    [LeetCode Sync] Runtime - 8 ms (1.96%), Memory - 42 MB (8.01%)

--- 0062-unique-paths/solution.java
+++ 0062-unique-paths/solution.java
@@ -1,25 +1,16 @@
-class Solution 
-{
-    public int uniquePaths(int m, int n) 
-    {
-        int[][] mat= new int[m][n];
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(i==0 || j==0)
-                {
-                    mat[i][j]=1;
-                }
-            }
-        }        
-        for(int i=1;i<m;i++)
-        {
-            for(int j=1;j<n;j++)
-            {
-                mat[i][j]=mat[i-1][j]+mat[i][j-1];
-            }
+class Solution {
+    public int paths(int m,int n,Map<String,Integer> map){
+         String str = m+","+n;
+        if(map.containsKey(str)){
+            return map.get(str);
         }
-        return mat[m-1][n-1];    
+        if(n==1 && m==1)return 1;
+        else if(m==0||n==0)return 0;
+        map.put(str,paths(m,n-1,map)+paths(m-1,n,map));
+        return map.get(str);
+    }
+    public int uniquePaths(int m, int n) {
+        int res=paths(m,n,new HashMap<>());
+        return res;
     }
 }
