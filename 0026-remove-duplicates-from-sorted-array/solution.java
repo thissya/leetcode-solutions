77192d7a7a0f2f165d2bd01ed50ee6cd71844504
Thissya <125356637+thissya@users.noreply.github.com>
2024-11-05 13:00:49 +0000

    [LeetCode Sync] Runtime - 1 ms (64.99%), Memory - 44.7 MB (64.11%)

--- 0026-remove-duplicates-from-sorted-array/solution.java
+++ 0026-remove-duplicates-from-sorted-array/solution.java
@@ -1,14 +1,12 @@
 class Solution {
-    public int removeDuplicates(int[] nums) 
-    {
-        int c=0;
-        for(int i=1;i<nums.length;i++)
-        {
-            if(nums[i]!=nums[c])
-            {
-                nums[++c]=nums[i];
+    public int removeDuplicates(int[] nums) {
+        int j=1;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]!=nums[i-1]){
+                nums[j]=nums[i];
+                j++;
             }
-        } 
-        return c+1;
+        }
+        return j;
     }
 }
