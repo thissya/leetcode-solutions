7093f24fe223bc443776a77bc08eccf4fd4d160b
Thissya <125356637+thissya@users.noreply.github.com>
2024-11-07 09:09:43 +0000

    [LeetCode Sync] Runtime - 4 ms (42.58%), Memory - 42.4 MB (99.97%)

--- 0496-next-greater-element-i/solution.java
+++ 0496-next-greater-element-i/solution.java
@@ -1,34 +1,25 @@
 class Solution {
-    public int[] nextGreaterElement(int[] nums1, int[] nums2) 
-    {
-        int[] ans=new int[nums1.length];
-        int ind=0;
-        for(int i=0;i<nums1.length;i++)
-        {
-            int max=0;
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] arr=new int[nums1.length];
+        int n=nums2.length;
+        for(int i=0;i<nums1.length;i++){
+            int cur=nums1[i];
             int f=0;
-            for(int j=0;j<nums2.length;j++)
-            {
-                if(nums1[i]==nums2[j])
-                {
-                    max=nums1[i];
+            int max=nums1[i];
+            for(int j=0;j<n;j++){
+                if(cur==nums2[j]){
                     f=1;
                 }
-                if(f==1 && max<nums2[j])
-                {
-                    max=nums2[j];
-                    break;
+                if(f==1){                    
+                    max=Math.max(max,nums2[j]);
+                    if(max>cur){
+                        break;
+                    }
                 }
             }
-            ans[ind++]=max;
-        }  
-        for(int i=0;i<ans.length;i++)
-        {
-            if(ans[i]==nums1[i])
-            {
-                ans[i]=-1;
-            }
-        } 
-        return ans;
+            if(max==nums1[i])max=-1;
+            arr[i]=max;
+        }
+        return arr;
     }
 }
