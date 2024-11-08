ba4e33d056e5fd00a28ed287bc5687a329461133
Thissya <125356637+thissya@users.noreply.github.com>
2024-11-08 04:48:45 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 45.2 MB (61.23%)

--- 0268-missing-number/solution.java
+++ 0268-missing-number/solution.java
@@ -1,12 +1,10 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int n=nums.length;
-        int sum=0;
-        for(int i=0;i<n;i++)
-        {
-            sum+=nums[i];
+        int sum1=0,sum2=0;
+        sum2=nums.length*(nums.length+1)/2;
+        for(int i=0;i<nums.length;i++){
+            sum1+=nums[i];
         }
-        int arrsum=(n*(n+1))/2;
-        return arrsum-sum;
+        return sum2-sum1;
     }
 }
