c66ed1862e02ef8126ae563dd55c2ffe232f1a3f
Thissya <125356637+thissya@users.noreply.github.com>
2024-11-10 07:30:41 +0000

    [LeetCode Sync] Runtime - 3 ms (38.99%), Memory - 44.9 MB (21.62%)

--- 3581-the-two-sneaky-numbers-of-digitville/solution.java
+++ 3581-the-two-sneaky-numbers-of-digitville/solution.java
@@ -1,16 +1,26 @@
 class Solution {
     public int[] getSneakyNumbers(int[] nums) {
-        int[] arr= new int[2];
-        int[] count= new int[100];
-        int j=0;
-        for(int i:nums)
-        {
-            count[i]++;
-            if(count[i]==2)
-            {
-                arr[j++]=i;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+        int[] arr = new int[2];
+        int i=0;
+        for(int n:map.keySet()){
+            if(map.get(n)>=2){
+                arr[i++]=n;
             }
         }
         return arr;
     }
 }
+
+/*
+        int[] arr=new int[2];
+        Arrays.sort(nums);
+        int j=0;
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]==nums[i+1])arr[j++]=nums[i];
+        }
+        return arr;    
+*/
