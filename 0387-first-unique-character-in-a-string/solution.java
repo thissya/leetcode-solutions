c00869cb53d387a0a43121331017bea0efdbd6d1
Thissya <125356637+thissya@users.noreply.github.com>
2024-12-23 20:24:56 +0000

    [LeetCode Sync] Runtime - 33 ms (27.73%), Memory - 45.6 MB (8.37%)

---
+++ 0387-first-unique-character-in-a-string/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+        int ans=-1;
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if(map.get(c)==1){
+                ans=i;
+                break;
+            }
+        }
+        return ans;
+    }
+}
