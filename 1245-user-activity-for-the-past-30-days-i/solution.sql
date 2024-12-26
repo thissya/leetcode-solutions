dea6864349f90936a6a6d3d70c718604c48ca3f9
Thissya <125356637+thissya@users.noreply.github.com>
2024-12-26 20:31:27 +0000

    [LeetCode Sync] Runtime - 768 ms (28.88%), Memory - 0B (100.00%)

--- 1245-user-activity-for-the-past-30-days-i/solution.sql
+++ 1245-user-activity-for-the-past-30-days-i/solution.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-select activity_date as day,count(distinct user_id) as active_users from Activity group by activity_date having activity_date<='2019-07-27' and activity_date>'2019-06-27';
+select activity_date as day ,count(distinct user_id) as active_users from Activity where activity_date between "2019-06-28" and "2019-07-27" group by activity_date;
