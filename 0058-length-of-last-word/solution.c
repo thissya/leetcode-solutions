33e8c7db6f392cc111992f99e9b216c896c89c35
Thissya <125356637+thissya@users.noreply.github.com>
2024-12-26 20:36:54 +0000

    [LeetCode Sync] Runtime - 3 ms (2.36%), Memory - 8 MB (64.51%)

--- 0058-length-of-last-word/solution.c
+++ 0058-length-of-last-word/solution.c
@@ -1,7 +1,6 @@
 int lengthOfLastWord(char* s) {
     int c=1;
-    for(int i=1;i<strlen(s);i++)
-    {
+    for(int i=1;i<strlen(s);i++){
         if(s[i]==' ')continue;
         if(s[i-1]==' ')c=0;
         c++;
