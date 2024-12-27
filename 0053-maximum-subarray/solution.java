1e098732f71499336553495cdaf5054d764c3ad4
Thissya <125356637+thissya@users.noreply.github.com>
2024-12-27 09:09:28 +0000

    [LeetCode Sync] Runtime - 1 ms (99.40%), Memory - 57.5 MB (7.70%)

--- 0053-maximum-subarray/solution.java
+++ 0053-maximum-subarray/solution.java
@@ -1,43 +1,13 @@
 class Solution {
-    public int maxSubArray(int[] nums) 
-    {
-        int max=nums[0];
+    public int maxSubArray(int[] nums) {
+        int n=nums.length;
         int cur=nums[0];
-        for(int i=1;i<nums.length;i++)
-        {
-            cur=Math.max(nums[i],cur+nums[i]);
-            max=Math.max(cur,max);
-        }    
+        int max=nums[0];
+        for(int i=1;i<n;i++){
+            cur=cur+nums[i];
+            if(cur<nums[i])cur=nums[i];
+            if(max<cur)max=cur;
+        }
         return max;
     }
 }
-/*
- public int maxSubArray(int[] nums) {
-        int len=nums[0];
-        if(nums.length==1)
-        {
-            return len;
-        }
-        for(int i=0;i<nums.length;i++)
-        {
-            for(int j=i+1;j<=nums.length;j++)
-            {
-                int temp=find(nums,i,j);
-                if(temp>len)
-                {
-                    len=temp;
-                }
-            }
-        }
-        return len;
-    }
-    public static int find(int[] nums,int s,int e)
-    {
-        int sum=0;
-        for(int i=s;i<e;i++)
-        {
-            sum+=nums[i];
-        }
-        return sum;
-    }
-    */
