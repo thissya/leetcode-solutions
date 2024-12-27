1f0f18d05b1304eb0b4deeec4b59a70a58b4322b
Thissya <125356637+thissya@users.noreply.github.com>
2024-12-27 20:40:40 +0000

    [LeetCode Sync] Runtime - 1651 ms (5.01%), Memory - 0B (100.00%)

--- 0619-biggest-single-number/solution.sql
+++ 0619-biggest-single-number/solution.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-select (select num  from MyNumbers group by num having count(num)=1 order by num desc limit 1) as num ;
+select (select num from MyNumbers group by num having count(num)=1 order by num desc limit 1)as num;
