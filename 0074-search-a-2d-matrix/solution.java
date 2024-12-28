2f147dc5a77bc6959891b8ad5ddc33eeba729001
Thissya <125356637+thissya@users.noreply.github.com>
2024-12-28 06:19:27 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.1 MB (56.56%)

--- 0074-search-a-2d-matrix/solution.java
+++ 0074-search-a-2d-matrix/solution.java
@@ -1,26 +1,18 @@
 class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) 
-    {
-        int n=matrix.length;
-        int m=matrix[0].length;
-        int low=0,high=(n*m)-1;
-        while(low<=high)
-        {
-            int mid=(low+high)/2;
-            int row=mid/m;
-            int col=mid%m;
-            if(matrix[row][col]==target)
-            {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int r=matrix.length;
+        int c=matrix[0].length;
+        int row=0;
+        int col=c-1;
+        while(row<r && col>=0){
+            if(matrix[row][col]==target){
                 return true;
+            }else if (matrix[row][col]<target){
+                row++;
+            }else{
+                col--;
             }
-            else if( matrix[row][col]<target)
-            {
-                low=mid+1;
-            }
-            else{
-                high=mid-1;
-            }
-        }
+        } 
         return false;
     }
 }
