0356090da87dd2042d1727425f48192019dd4fa0
Thissya <125356637+thissya@users.noreply.github.com>
2024-12-28 12:55:40 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.3 MB (48.30%)

--- 0242-valid-anagram/solution.c
+++ 0242-valid-anagram/solution.c
@@ -1,20 +1,14 @@
 bool isAnagram(char* s, char* t) {
-    int n=strlen(s);
-    int n1=strlen(t);
-    if(n!=n1)
-    {
-        return false;
-    }
+    int n=strlen(t);
+    int m=strlen(s);
+    if(n!=m)return false;
     int arr[126]={0};
-    for(int i=0;i<n;i++)
-    {
+    for(int i=0;i<n;i++){
         arr[s[i]]++;
         arr[t[i]]--;
     }
-    for(int i=0;i<126;i++)
-    {
-        if(arr[i]!=0)
-        {
+    for(int i=0;i<n;i++){
+        if(arr[s[i]]!=0){
             return false;
         }
     }
