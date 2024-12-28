f8513161a21f5ad354877db753943c2b53c161dd
Thissya <125356637+thissya@users.noreply.github.com>
2024-12-28 04:03:16 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.1 MB (96.54%)

---
+++ 0792-binary-search/solution.c
@@ -0,0 +1,16 @@
+int search(int* nums, int n, int target) {
+    int l=0;
+    int r= n-1;
+    while(l<=r){
+        int mid=(l+r)/2;
+        if(nums[mid]==target){
+            return mid;
+        }
+        else if(nums[mid]<target){
+            l=mid+1;
+        }else{
+            r=mid-1;
+        }
+    }
+    return -1;
+}
