40e72b0ee7141d9c53bdc8868aa59e9f5dced2a2
Thissya <125356637+thissya@users.noreply.github.com>
2024-12-28 18:46:36 +0000

    [LeetCode Sync] Runtime - 6 ms (97.78%), Memory - 48 MB (38.49%)

--- 0049-group-anagrams/solution.java
+++ 0049-group-anagrams/solution.java
@@ -1,41 +1,55 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> res= new ArrayList<>();
-        for(String temp:strs)
-        {
-            List<String> temp_res=anagram(temp,strs);
-            if(!res.contains(temp_res))
-            {
-                res.add(temp_res);
-            }  
+        HashMap<String,List<String>> map = new HashMap<>();
+        for(String s:strs){
+            char[] arr=s.toCharArray();
+            Arrays.sort(arr);
+            String str= new String(arr);
+            List<String> li= map.getOrDefault(str,new ArrayList<>());
+            li.add(s);  
+            map.put(str,li);
         }
+        List<List<String>> res=new ArrayList<>(map.values());
         return res;
     }
-    public static List<String> anagram(String temp,String[] strs)
-    {
-        List<String> temp_res=new ArrayList<>();
-        for(String s:strs)
-        {
-            if(isanagram(temp,s))
-            {
-                temp_res.add(s);
-            }
-        }
-        return temp_res;
-    }
-    public static boolean isanagram(String s1,String s2)
-    {
-        if(s1.length()!=s2.length())return false;
-        int[] arr=new int[26];
-        for(int i=0;i<s1.length();i++)
-        {
+}
+
+
+
+/*
+Approach 1:
+
+    public  static boolean anagram(String s1,String s2){
+        int n=s1.length();
+        int m=s2.length();
+        if(n!=m)return false;
+        int[] arr= new int[26];
+        for(int i=0;i<n;i++){
             arr[s1.charAt(i)-'a']++;
             arr[s2.charAt(i)-'a']--;
         }
-        for(int i:arr)
-        {
-            if(i!=0)return false;
+        for(int i=0;i<26;i++){
+            if(arr[i]!=0)return false;
         }
         return true;
     }
-}
+    public static List<String> find(String s,String[] strs){
+        List<String> arr=new ArrayList<>();
+        for(String str:strs){
+            if(anagram(str,s)){
+                arr.add(str);
+            }
+        }
+        return arr;
+    }
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> res = new ArrayList<>();
+        for(String s:strs){
+            List<String> arr = find(s,strs);
+            if(!res.contains(arr)){
+                res.add(arr);
+            } 
+        }
+        return res;
+    }
+*/
