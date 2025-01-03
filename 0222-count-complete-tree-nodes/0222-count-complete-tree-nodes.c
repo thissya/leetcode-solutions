f6f618c747921479aa10352c250172a0cb38d074
Thissya <125356637+thissya@users.noreply.github.com>
2025-01-03 14:36:02 +0530

    Time: 0 ms (100%), Space: 20.1 MB (74.29%) - LeetHub

---
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.c
@@ -0,0 +1,12 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+int countNodes(struct TreeNode* root) {
+    if(root==NULL)return 0;
+    return 1+countNodes(root->left)+countNodes(root->right);    
+}
