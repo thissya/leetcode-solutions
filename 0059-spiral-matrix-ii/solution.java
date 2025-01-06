9a354d9d178efab740559c2fc7042320bdf20d0d
Thissya <125356637+thissya@users.noreply.github.com>
2025-01-06 14:07:37 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.5 MB (53.74%)

--- 0059-spiral-matrix-ii/solution.java
+++ 0059-spiral-matrix-ii/solution.java
@@ -4,35 +4,28 @@ class Solution {
         int val=1;
         int left=0,right=n-1;
         int top=0,bottom=n-1;
-        while(left<=right && top <=bottom)
-        {
-            for(int i=left;i<=right;i++)
-            {
+        while(top<=bottom && left<=right){
+            for(int i=left;i<=right;i++){
                 arr[top][i]=val++;
             }
             top++;
-            for(int i=top;i<=bottom;i++)
-            {
+            for(int i=top;i<=bottom;i++){
                 arr[i][right]=val++;
             }
             right--;
-            if(top<=bottom)
-            {
-                for(int i=right;i>=left;i--)
-                {
+            if(top<=bottom){
+                for(int i=right;i>=left;i--){
                     arr[bottom][i]=val++;
                 }
+                bottom--;
             }
-            bottom--;
-            if(left<=right)
-            {
-                for(int i=bottom;i>=top;i--)
-                {
+            if(left<=right){
+                for(int i=bottom;i>=top;i--){
                     arr[i][left]=val++;
                 }
+                left++;
             }
-            left++;
-        }
+        }    
         return arr;
     }
 }
