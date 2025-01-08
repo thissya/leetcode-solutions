ffe51e2cc7874eb3e9a2dd99b7f995422558aee1
Thissya <125356637+thissya@users.noreply.github.com>
2025-01-08 14:22:13 +0000

    [LeetCode Sync] Runtime - 155 ms (40.75%), Memory - 41.4 MB (80.53%)

--- 0079-word-search/solution.java
+++ 0079-word-search/solution.java
@@ -1,38 +1,34 @@
 class Solution {
+    public boolean check(int i,int j,int r,int c,int ind,char[][] board, String word , int[][] visited){
+        if(ind==word.length())return true;
+        if(i<0 || i>=r ||j<0 || j>=c){
+            return false;
+        }
+        if(board[i][j]!=word.charAt(ind))return false;
+        if(visited[i][j]==1)return false;
+        
+        visited[i][j]=1;
+        if(check(i+1,j,r,c,ind+1,board,word,visited)||check(i-1,j,r,c,ind+1,board,word,visited) || check(i,j+1,r,c,ind+1,board,word,visited)||check(i,j-1,r,c,ind+1,board,word,visited)){
+            return true;
+        }
+        visited[i][j]=0;
+        return false;
+    }
     public boolean exist(char[][] board, String word) {
-        int[][] visited= new int[board.length][board[0].length];
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[0].length;j++){
-                if(board[i][j]==word.charAt(0)){
-                    boolean res= recur(board,word,i,j,0,visited);
-                    if(res)return true;
+        int r=board.length;
+        int c=board[0].length;
+        int[][] visited= new int[r][c];
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                char ch=board[i][j];
+                if(ch==word.charAt(0)){
+                    boolean a=check(i,j,r,c,0,board,word,visited);
+                    if(a==true){
+                        return true;
+                    }
                 }
             }
         }
         return false;
     }
-
-    public boolean recur(char[][] board ,String word , int i,int j,int ind,int[][] visited){
-        if(ind == word.length() )return true;
-        boolean exp1 = (0<=i && i<board.length);
-        boolean exp2 = (0<=j && j<board[0].length);
-        
-        if(!exp1 || !exp2) return false;
-        if(visited[i][j]==1)return false;
-        if(board[i][j]!=word.charAt(ind))return false;
-        
-        visited[i][j]=1;
-
-        if(recur(board,word,i+1,j,ind+1,visited) || 
-           recur(board,word,i-1,j,ind+1,visited) || 
-           recur(board,word,i,j+1,ind+1,visited) || 
-           recur(board,word,i,j-1,ind+1,visited))
-        {
-            return true;
-        }
-        visited[i][j]=0;
-        return false; 
-    }
-
-
 }
