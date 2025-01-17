1d82477d115f02d8aa119102b1e45a39e2baaa69
Thissya <125356637+thissya@users.noreply.github.com>
2025-01-17 14:07:08 +0000

    [LeetCode Sync] Runtime - 1 ms (82.03%), Memory - 43.5 MB (27.97%)

--- 0022-generate-parentheses/solution.java
+++ 0022-generate-parentheses/solution.java
@@ -1,28 +1,20 @@
 class Solution {
-    private ArrayList<String> res= new ArrayList<>();
-    public void  generate(int n,String str){
-        if(n==0){
-            Stack<Character> stack= new Stack<>();
-            for(int i=0;i<str.length();i++){
-                char temp=str.charAt(i);
-                if(temp=='('){
-                    stack.push(temp);
-                }else if(stack.isEmpty()){
-                    return ;
-                }
-                else{
-                    stack.pop();
-                }
-            }
-            if(stack.isEmpty())
-                res.add(str);
-        }else{
-            generate(n-1,str+"(");
-            generate(n-1,str+")");
+    public void recur(int open,int close,int n,String curr,List<String> res){
+        if(curr.length()==n*2){
+            res.add(curr);
+            return ;
         }
+        if(open<n){
+            recur(open+1,close,n,curr+'(',res);
+        }
+        if(close<open){
+            recur(open,close+1,n,curr+')',res);
+        }
+
     }
     public List<String> generateParenthesis(int n) {
-        generate(n*2,"");
+        List<String> res= new ArrayList<>();
+        recur(0,0,n,"",res);
         return res;
     }
 }
