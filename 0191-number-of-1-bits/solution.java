63aaf10dea4f6b14a256d6b7c822bf0b819f2860
Thissya <125356637+thissya@users.noreply.github.com>
2025-01-28 12:36:54 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.1 MB (22.61%)

--- 0191-number-of-1-bits/solution.java
+++ 0191-number-of-1-bits/solution.java
@@ -1,16 +1,11 @@
-class Solution 
-{
-    public int hammingWeight(int n) 
-    {
+class Solution {
+    public int hammingWeight(int n) {
         int c=0;
-        while(n!=0)
-        {
-            if((n&1) ==1)
-            {
-                c++;
-            }
-            n=n>>1;
-        }        
+        while(n>0){
+            int rem=(n%2==0 ?0:1);
+            c+=rem;
+            n/=2;
+        }
         return c;
     }
 }
