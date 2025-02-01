04302a73c4f5e469c5ccb0d03e92cbcc1ed7dd5d
Thissya <125356637+thissya@users.noreply.github.com>
2025-02-01 05:28:25 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.7 MB (53.82%)

--- 3429-special-array-i/solution.c
+++ 3429-special-array-i/solution.c
@@ -1,11 +1,20 @@
-bool isArraySpecial(int* nums, int numsSize) {
-    for(int i=0;i<numsSize-1;i++)
-    {
-        if((nums[i]&1) == (nums[i+1]&1))
-        {
-            return false;
+bool isArraySpecial(int* nums, int n) {
+    int odd=0;
+    int even=0;
+    for(int i=0;i<n;i++){
+        if(nums[i]%2==0){
+            if(even==1){
+                return false;
+            }
+            even=1;
+            odd=0;
+        }else{
+            if(odd==1){
+                return false;
+            }
+            odd=1;
+            even=0;
         }
     }
     return true;
-    
 }
