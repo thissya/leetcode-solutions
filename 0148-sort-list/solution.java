acb53e6fded64227bdce56576dfba7496c72758e
Thissya <125356637+thissya@users.noreply.github.com>
2025-03-26 16:00:57 +0000

    [LeetCode Sync] Runtime - 11 ms (37.54%), Memory - 56.6 MB (67.48%)

--- 0148-sort-list/solution.java
+++ 0148-sort-list/solution.java
@@ -8,31 +8,70 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-import java.util.*;
 class Solution {
-    public ListNode sortList(ListNode head) 
-    {
-        int[] arr= new int[100000];
-        int n=0;    
-        ListNode temp=head;
-        while(temp!=null)
-        {
-            arr[n++]=temp.val;
-            temp=temp.next;
-        }
-        int[] arr1= new int[n];
-        for(int i=0;i<n;i++)
-        {
-            arr1[i]=arr[i];
-        }
-        Arrays.sort(arr1);
-        temp=head;
-        int i=0;
-        while(temp!=null)
-        {
-            temp.val=arr1[i++];
-            temp=temp.next;
-        }
-        return head;
+    public ListNode sortList(ListNode head) {
+        return split(head);
     }
+    
+    public ListNode split(ListNode head){
+        if(head ==null || head.next == null){
+            return head;
+        }
+        ListNode mid = findMid(head);
+        ListNode left = split(head);
+        ListNode right = split(mid);
+        return merge(left,right);
+    }
+
+    public ListNode merge(ListNode left ,ListNode right){
+        ListNode res =null,cur=null;
+
+        while(left!=null && right!=null){
+            if(left.val<right.val){
+                if(res==null){
+                    res=left;
+                    cur=left;
+                }else{
+                    cur.next = left;
+                    cur=cur.next;
+                }
+                left=left.next;
+            }else{
+                if(res==null){
+                    res=right;
+                    cur=right;
+                }else{
+                    cur.next=right;
+                    cur=cur.next;
+                }
+                right = right.next;
+            }
+        }
+
+        while(left!=null){
+            cur.next=left;
+            cur=cur.next;
+            left=left.next;
+        }
+        while(right!=null){
+            cur.next=right;
+            cur=cur.next;
+            right=right.next;
+        }
+        return res;
+    }
+
+    public ListNode findMid(ListNode head){
+        ListNode prev=head;
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast!=null && fast.next!=null){
+            fast=fast.next.next;
+            prev=slow;
+            slow=slow.next;
+        }
+        prev.next=null;
+        return slow;
+    }
+
 }
