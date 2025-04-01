6aaf6a46ae7f110561e630f2ba9455d5571eb48b
Thissya <125356637+thissya@users.noreply.github.com>
2025-04-01 09:00:52 +0000

    [LeetCode Sync] Runtime - 486 ms (99.89%), Memory - 0.0B (100.00%)

--- 1135-customers-who-bought-all-products/solution.sql
+++ 1135-customers-who-bought-all-products/solution.sql
@@ -1,2 +1,1 @@
-# Write your MySQL query statement below
-select customer_id from Customer group by customer_id having count(distinct product_key)=(select count(product_key) from Product);
+select customer_id from Customer group by customer_id having count(distinct(product_key)) = (select count(product_key) from Product);
