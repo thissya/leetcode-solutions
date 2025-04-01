39f0fe09e359d4404ee98bdc64708694c6b03a6f
Thissya <125356637+thissya@users.noreply.github.com>
2025-04-01 16:20:12 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 45.8 MB (6.16%)

--- 0783-search-in-a-binary-search-tree/solution.java
+++ 0783-search-in-a-binary-search-tree/solution.java
@@ -16,8 +16,12 @@
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
         if(root==null)return null;
+
         if(root.val==val)return root;
-        else if(root.val>val) return searchBST(root.left,val);
-        else return searchBST(root.right,val);        
+        else if(root.val<val){
+            return searchBST(root.right,val);
+        }else{
+            return searchBST(root.left,val);
+        }
     }
 }
