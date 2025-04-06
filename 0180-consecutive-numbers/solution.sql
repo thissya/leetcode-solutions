b42758c46693f14fe8080ea175f479e87e527af6
Thissya <125356637+thissya@users.noreply.github.com>
2025-04-06 18:23:02 +0000

    [LeetCode Sync] Runtime - 522 ms (96.32%), Memory - 0.0B (100.00%)

--- 0180-consecutive-numbers/solution.sql
+++ 0180-consecutive-numbers/solution.sql
@@ -1,3 +1,2 @@
 # Write your MySQL query statement below
-select distinct l1.num as ConsecutiveNums from Logs l1 join Logs l2 on l1.id=l2.id-1 join Logs l3 on l1.id=l3.id-2 
-where l1.num=l2.num and l2.num =l3.num ; 
+select distinct l1.num as ConsecutiveNums from Logs l1 join Logs l2 on l1.id =l2.id+1 join Logs l3 on l1.id=l3.id+2 where l1.num=l2.num and l1.num=l3.num; 
