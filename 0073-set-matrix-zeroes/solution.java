8b68f69ee1761cf7a9f1ffbee235a4d32b103726
Thissya <125356637+thissya@users.noreply.github.com>
2025-05-21 08:35:14 +0000

    [LeetCode Sync] Runtime - 1 ms (72.39%), Memory - 46 MB (7.86%)

--- 0073-set-matrix-zeroes/solution.java
+++ 0073-set-matrix-zeroes/solution.java
@@ -1,33 +1,33 @@
-class Solution 
-{
-    public void setZeroes(int[][] matrix) 
-    {
-        int n=matrix.length;
-        int m=matrix[0].length;
-        int row[]=new int [n];
-        int col[]=new int [m];
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(matrix[i][j]==0)
-                {
-                    row[i]=1;
-                    col[j]=1;
-                }
-            }
+class Solution {
+    public void recur1(int row,int col,int[][] mat,int c){
+        if(col>=c){
+            return ;
         }
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(row[i]==1 || col[j]==1)
-                {
-                    matrix[i][j]=0;
-                }
-                
-            }
+        mat[row][col]=0;
+        recur1(row,col+1,mat,c);
+    }
+    public void recur2(int row,int col,int[][] mat,int r){
+        if(row>=r){
+            return ;
         }
-        
+        mat[row][col]=0;
+        recur2(row+1,col,mat,r);
+    }
+    public void setZeroes(int[][] mat) {
+        List<int[]> list = new ArrayList<>();
+        int r=mat.length;
+        int c=mat[0].length;
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(mat[i][j]==0)
+                    list.add(new int[] {i,j});
+            }
+        }   
+        for(int[] cur:list){
+            int i=cur[0];
+            int j=cur[1];
+            recur1(i,0,mat,c);
+            recur2(0,j,mat,r);
+        }
     }
 }
