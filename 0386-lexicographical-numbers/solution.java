26021ac9d69d8e1df708bd16aa5f5b9832a35ea7
Thissya <125356637+thissya@users.noreply.github.com>
2025-06-08 17:22:57 +0000

    [LeetCode Sync] Runtime - 4 ms (66.79%), Memory - 48.7 MB (29.95%)

--- 0386-lexicographical-numbers/solution.java
+++ 0386-lexicographical-numbers/solution.java
@@ -1,16 +1,35 @@
 class Solution {
     public List<Integer> lexicalOrder(int n) {
-        String[] arr= new String[n];
-        for(int i=1;i<=n;i++)
-        {
-            arr[i-1] = String.valueOf(i);
+        List<Integer> res = new ArrayList<>();
+        for(int i=1;i<=9;i++){
+            dfs(i,n,res);
         }
+        return res;
+    }
+    public static void dfs(int current, int n , List<Integer> res){
+        if(current>n) return ;
+        res.add(current);
+        for(int i=0;i<=9;i++){
+            int next = current*10 + i;
+            //if(next>n) return ;
+            dfs(next,n,res);
+        }
+    }
+}
+
+/*
+public List<Integer> lexicalOrder(int n) {
+        String[] arr = new String[n];
+        List<Integer> res = new ArrayList<>();
+        for(int i=1;i<=n;i++){
+            arr[i-1] = String.valueOf(i);
+        }    
         Arrays.sort(arr);
-        List<Integer> res= new ArrayList<>();
-        for(String str:arr)
-        {
+        for(String str:arr){
             res.add(Integer.valueOf(str));
         }
         return res;
     }
-}
+
+*/
+
