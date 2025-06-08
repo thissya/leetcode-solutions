5357d1360de0a884e4e5c226a42850a092978523
Thissya <125356637+thissya@users.noreply.github.com>
2025-06-08 17:38:33 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.3 MB (79.65%)

--- 0371-sum-of-two-integers/solution.java
+++ 0371-sum-of-two-integers/solution.java
@@ -1,5 +1,10 @@
 class Solution {
     public int getSum(int a, int b) {
-        return a+b;
+        while(b!=0){
+            int temp = (a&b)<<1;
+            a = a^b;
+            b=temp;
+        }
+        return a;
     }
 }
