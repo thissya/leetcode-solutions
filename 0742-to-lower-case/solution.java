fd5ad174af47d865912835584eb0e7b3bfbb43d1
Thissya <125356637+thissya@users.noreply.github.com>
2025-06-12 06:44:58 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.6 MB (61.23%)

---
+++ 0742-to-lower-case/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+}
+
+/*
+int n =s.length();
+        String str ="";
+        for(int i=0;i<n;i++){
+            if(s.charAt(i)>=65 && s.charAt(i)<=90){
+                str+=(char)(s.charAt(i)+32);
+            }
+            else{
+                str+=s.charAt(i);
+            }
+        }
+        return str;
+*/
