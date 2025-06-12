914a39c632b098fbf965c263eeb41200c90d160a
Thissya <125356637+thissya@users.noreply.github.com>
2025-06-12 06:21:44 +0000

    [LeetCode Sync] Runtime - 1 ms (100.00%), Memory - 44.4 MB (83.35%)

---
+++ 0002-add-two-numbers/solution.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        int c=0;
+        while(l1!=null || l2!=null || c!=0){
+            int sum=c;
+            if(l1!=null){
+                sum+=l1.val;
+                l1=l1.next;
+            }
+            if(l2!=null){
+                sum+=l2.val;
+                l2=l2.next;
+            }
+            c=sum/10;
+            int rem = sum%10;
+            ListNode newnode= new ListNode(rem);
+            cur.next= newnode;
+            cur=newnode;
+        }
+        return dummy.next;
+    }
+}
