8362043951fe193fd3ddc0448746055730ce6c40
Thissya <125356637+thissya@users.noreply.github.com>
2025-06-13 04:32:44 +0000

    [LeetCode Sync] Runtime - 5 ms (16.75%), Memory - 43.4 MB (53.20%)

--- 0040-combination-sum-ii/solution.java
+++ 0040-combination-sum-ii/solution.java
@@ -1,30 +1,60 @@
 class Solution {
-    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        Arrays.sort(candidates);
-        List<List<Integer>> ans= new ArrayList<>();
-        backtrack(ans,new ArrayList<>(),candidates,target,0);
-        return ans;
+    public List<List<Integer>> combinationSum2(int[] can, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(can);
+        backtrack(0,new ArrayList<>(),target,can,res);
+        return res;
     }
-    public void backtrack(List<List<Integer>> ans,List<Integer> temp,int[] can,int remain,int start)
-    {
-        if(remain==0)
-        {
-            ans.add(new ArrayList<>(temp));
+    public void backtrack(int start,List<Integer> cur,int target,int[] can,List<List<Integer>> res){
+        if(target<0){
             return ;
         }
-        else if(remain<0)return ;
-        else
-        {
-            for(int i=start;i<can.length;i++)
-            {
-                if(i>start && can[i]==can[i-1])continue;
-                if(remain>=can[i])
-                {
-                    temp.add(can[i]);
-                    backtrack(ans,temp,can,remain-can[i],i+1);
-                    temp.remove(temp.size()-1);
-                }
+        if(target==0){
+            if(!res.contains(cur)){
+                res.add(new ArrayList<>(cur));
             }
+            return ;
         }
+
+        for(int i=start;i<can.length;i++){
+            if(i>start && can[i]==can[i-1])continue;
+            cur.add(can[i]);
+            backtrack(i+1,cur,target-can[i],can,res);
+            cur.remove(cur.size()-1);
+        }
+
     }
 }
+
+/*
+
+Time Limit Exceed
+public List<List<Integer>> combinationSum2(int[] can, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        boolean[] flag = new boolean[can.length];
+        Arrays.sort(can);
+        backtrack(0,new ArrayList<>(),target,can,res,flag);
+        return res;
+    }
+    public void backtrack(int start,List<Integer> cur,int target,int[] can,List<List<Integer>> res,boolean[] flag){
+        if(target<0){
+            return ;
+        }
+        if(target==0){
+            if(!res.contains(cur)){
+                res.add(new ArrayList<>(cur));
+            }
+            return ;
+        }
+
+        for(int i=start;i<can.length;i++){
+            if(flag[i])continue;
+            cur.add(can[i]);
+            flag[i]=true;
+            backtrack(i+1,cur,target-can[i],can,res,flag);
+            flag[i]=false;
+            cur.remove(cur.size()-1);
+        }
+
+    }
+*/
