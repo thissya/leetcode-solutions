8a02fba27002ab9ca1f0324e6c6a523b0a50cca4
Thissya <125356637+thissya@users.noreply.github.com>
2025-06-16 15:56:19 +0000

    [LeetCode Sync] Runtime - 1 ms (50.89%), Memory - 43 MB (18.11%)

--- 0078-subsets/solution.java
+++ 0078-subsets/solution.java
@@ -1,47 +1,20 @@
 class Solution {
-    // construct and view tree how it works
-    List<List<Integer>> res = new ArrayList<>();
-    public void dfs(int[] nums,int i,int n,List<Integer> arr){
-        if(i>=n){
-            res.add(arr);
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        int n = nums.length;
+        recur(0,res,new ArrayList<>(),nums,n);
+        return res;
+    }
+    
+    public static void recur(int i,List<List<Integer>> res, List<Integer> cur,int[] nums,int n){
+        if(i==n){
+            res.add(cur);
             return;
         }
-        List<Integer> newarr = new ArrayList<>(arr);
+        List<Integer> newarr =  new ArrayList<>(cur);
         newarr.add(nums[i]);
-        dfs(nums,i+1,n,newarr);
-        dfs(nums,i+1,n,arr);
-    }
-
-    public List<List<Integer>> subsets(int[] nums) {
-        int n = nums.length;
-        List<Integer> arr = new ArrayList<>();
-        dfs(nums,0,n,arr);
-        return res;
+        recur(i+1,res,newarr,nums,n);
+        recur(i+1,res,cur,nums,n);
     }
 
 }
-/*
-    public List<List<Integer>> subsets(int[] nums) {
-        // bitwise method 
-        //  001 -1
-        //  010 -2  011-[2,3]
-        //  100 -3
-        //  101 -[1,3] 110 -[1,2]    
-        
-        int n = nums.length;
-        int pow = 1<<n;
-        List<List<Integer>> res = new ArrayList<>();
-        for(int i=0;i<pow;i++){
-            List<Integer> temp = new ArrayList<>();
-            for(int j=0;j<n;j++){
-                if((i&(1<<j)) !=0){
-                    temp.add(nums[j]);
-                }
-                cur=cur>>1;
-            }
-            res.add(temp);
-        }
-        return res;
-    }
-
-*/
