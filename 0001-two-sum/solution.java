4baf0501a1494602cdbc4828851cee0817ed2586
Thissya <125356637+thissya@users.noreply.github.com>
2025-06-18 04:15:11 +0000

    [LeetCode Sync] Runtime - 10 ms (48.64%), Memory - 45 MB (46.85%)

--- 0001-two-sum/solution.java
+++ 0001-two-sum/solution.java
@@ -1,16 +1,62 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        int[] res= new int[2];
-        for(int i=0;i<nums.length;i++){
-            if(map.containsKey(target-nums[i])){
-                res[0]=map.get(target-nums[i]);
-                res[1]=i;
+        
+        int sum=0;
+        int l=0,n=nums.length;
+        int r=n-1;
+        int[] nums2=new int[n];
+        for(int i=0;i<n;i++){
+            nums2[i]=nums[i];
+        }
+        Arrays.sort(nums);
+        int left=0;
+        int right=0;
+        int[] res = {-1,-1};
+        while(l<r){
+            sum=nums[l]+nums[r];
+            if(sum>target){
+                r--;
+            }
+            else if(sum<target){
+                l++;
+            }
+            if(sum==target){
+                left=nums[l];
+                right=nums[r];
                 break;
-            }else{
-                map.put(nums[i],i);
             }
         }
+
+        boolean le =true;
+        System.out.println(left+" "+right);
+        for(int i=0;i<n;i++){
+            if(left ==nums2[i] || right==nums2[i]){
+                if(le){
+                    res[0]=i;
+                    le=false;
+                }else{
+                    res[1]=i;
+                }
+            }
+        }
+
         return res;
     }
 }
+
+/*
+    
+        int n = nums.length;
+        List<Integer> arr = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            arr.add(nums[i]);
+        }
+        for(int i=0;i<n;i++){
+            int cur = target-nums[i];
+            if(arr.contains(cur) && arr.indexOf(cur)!=i){
+                return new int[]{i,arr.indexOf(cur)};
+            }
+        }
+        return new int[]{-1,-1};
+        
+*/
