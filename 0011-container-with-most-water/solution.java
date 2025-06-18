980e1a060fe32f2b5f6c1e9393c1029983199bc0
Thissya <125356637+thissya@users.noreply.github.com>
2025-06-18 06:39:15 +0000

    [LeetCode Sync] Runtime - 5 ms (75.48%), Memory - 57.8 MB (64.42%)

--- 0011-container-with-most-water/solution.java
+++ 0011-container-with-most-water/solution.java
@@ -1,21 +1,16 @@
 class Solution {
-    public int maxArea(int[] height) 
-    {
-        int ans=0;
-        int l=0;
-        int r=height.length-1;
-        while(l<r)
-        {
+    public int maxArea(int[] height) {
+        int l=0,r=height.length-1;
+        int max=0;
+        while(l<r){
             int cur=Math.min(height[l],height[r])*(r-l);
-            ans=Math.max(ans,cur);
-            if(height[l]<height[r])
-            {
+            max=Math.max(max,cur);
+            if(height[l]<height[r]){
                 l++;
-            }
-            else{
+            }else{
                 r--;
             }
         }
-        return ans;
+        return max;
     }
 }
