45bedf2abbbafcd4686a2094ea5503e3705464e8
Thissya <125356637+thissya@users.noreply.github.com>
2025-06-19 14:58:19 +0000

    [LeetCode Sync] Runtime - 1 ms (1.20%), Memory - 41.2 MB (53.22%)

--- 0100-same-tree/solution.java
+++ 0100-same-tree/solution.java
@@ -15,30 +15,64 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        Queue<TreeNode> que1 = new LinkedList<>();
-        Queue<TreeNode> que2 = new LinkedList<>();
-        TreeNode temp=p;
-        TreeNode temp2=q;
-        que1.add(temp);
-        que2.add(temp2);
-        while(!que1.isEmpty()|| !que2.isEmpty()){
-            temp=que1.remove();
-            temp2=que2.remove();
-            if(temp == null && temp2!=null){
-                return false;
-            }else if(temp !=null && temp2==null){
+        List<Integer> arr1 = bfs(p);
+        List<Integer> arr2 = bfs(q);
+        return arr1.equals(arr2);
+    }
+    public List<Integer> bfs(TreeNode root){
+        Queue<TreeNode> q= new LinkedList<>();
+        q.add(root);
+        List<Integer> temp = new ArrayList<>();
+        while(!q.isEmpty()){
+            int c = q.size();
+            while(c>0){
+                c--;
+                TreeNode cur = q.poll();
+                if(cur==null){
+                    temp.add(1000001);
+                }else{
+                    temp.add(cur.val);
+                    q.add(cur.left);
+                    q.add(cur.right);
+                }
+            }
+        }
+        return temp;
+    }
+}
+
+/*
+
+public boolean isSameTree(TreeNode p, TreeNode q) {
+        Queue<TreeNode> t1 = new LinkedList<>();
+        Queue<TreeNode> t2 = new LinkedList<>();
+        
+        t1.add(p);
+        t2.add(q);
+
+        while(!t1.isEmpty() || !t2.isEmpty()){
+            TreeNode cur1 = t1.poll();
+            TreeNode cur2 = t2.poll();
+            if(cur1==null && cur2==null){
+                continue;
+            }
+            else if(cur1!=null && cur2==null){
                 return false;
             }
-            else if(temp==null && temp2==null)continue;
-            if(temp.val!=temp2.val){
+            else if(cur1==null && cur2!=null){
                 return false;
             }
-            que1.add(temp.left);
-            que1.add(temp.right);
-            que2.add(temp2.left);
-            que2.add(temp2.right);
-            
+
+            if(cur1.val!=cur2.val){
+                return false;
+            }
+            t1.add(cur1.left);
+            t1.add(cur1.right);
+            t2.add(cur2.left);
+            t2.add(cur2.right);
         }
         return true;
     }
-}
+
+*/
+
