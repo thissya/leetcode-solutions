fa44b10724627790da9ace22673e4f18e06dae58
Thissya <125356637+thissya@users.noreply.github.com>
2025-06-19 09:11:06 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.4 MB (25.51%)

--- 0048-rotate-image/solution.java
+++ 0048-rotate-image/solution.java
@@ -1,32 +1,28 @@
 class Solution {
-    public void rotate(int[][] matrix) 
-    {
-        int n=matrix.length;
-        for(int i=0;i<n;i++)
-        {
-            for(int j=i;j<n;j++)
-            {
-                int temp=matrix[i][j];
-                matrix[i][j]=matrix[j][i];
-                matrix[j][i]=temp;
+    public void rotate(int[][] mat) {
+        int n = mat.length;
+        int m = mat[0].length;
+
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<m;j++){
+                int temp = mat[i][j];
+                mat[i][j]=mat[j][i];
+                mat[j][i]=temp;
             }
         }
 
-        for(int i=0;i<n;i++)
-        {
-            int left=0,right=n-1;
-            while(left<right)
-            {
-                int temp=matrix[i][left];
-                matrix[i][left]=matrix[i][right];
-                matrix[i][right]=temp;
-                left++;
-                right--;
+        for(int i=0;i<n;i++){
+            int l=0;
+            int[] temp = mat[i];
+            int r = temp.length-1;
+            while(l<r){
+                int cur=temp[l];
+                temp[l]=temp[r];
+                temp[r]=cur;
+                l++;
+                r--;
             }
         }
-        for(int[] r:matrix)
-        {
-            System.out.println(Arrays.toString(r));
-        }
+
     }
 }
