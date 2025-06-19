45e41de28d0df7da90d81641e4d32fc83b03782f
Thissya <125356637+thissya@users.noreply.github.com>
2025-06-19 13:23:59 +0000

    [LeetCode Sync] Runtime - 1 ms (71.25%), Memory - 42.1 MB (62.20%)

--- 0199-binary-tree-right-side-view/solution.java
+++ 0199-binary-tree-right-side-view/solution.java
@@ -15,16 +15,18 @@
  */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> res= new ArrayList<>();
-        Queue<TreeNode> q= new LinkedList<>();
-        if(root==null)return res; 
+        List<Integer> res = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        if(root==null){
+            return res;
+        }
         q.add(root);
         while(!q.isEmpty()){
-            int c=q.size();
-            List<Integer> arr= new ArrayList<>();
-            while(c!=0){
-                c--;
-                TreeNode cur= q.poll();
+            int n=q.size();
+            List<Integer> arr = new ArrayList<>();
+            while(n!=0){
+                n--;
+                TreeNode cur = q.poll();
                 arr.add(cur.val);
                 if(cur.left!=null)q.add(cur.left);
                 if(cur.right!=null)q.add(cur.right);
