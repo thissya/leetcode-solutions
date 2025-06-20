d78a542359e338d9a896a1e96813366edb625404
Thissya <125356637+thissya@users.noreply.github.com>
2025-06-20 09:40:38 +0000

    [LeetCode Sync] Runtime - 59 ms (5.13%), Memory - 68.4 MB (21.51%)

--- 3501-delete-nodes-from-linked-list-present-in-array/solution.java
+++ 3501-delete-nodes-from-linked-list-present-in-array/solution.java
@@ -10,24 +10,22 @@
  */
 class Solution {
     public ListNode modifiedList(int[] nums, ListNode head) {
-        HashSet<Integer> arr= new HashSet<>();
-        for(int i:nums)
-        {
-            arr.add(i);
-        }
+        Set<Integer> set = new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            set.add(nums[i]);
+        }   
+        System.out.println(set); 
         ListNode dummy = new ListNode(0);
         dummy.next=head;
-        ListNode cur;
-        cur=dummy;
-        while(cur!=null && cur.next!=null)
-        {
-            if(arr.contains(cur.next.val))
-            {
-                cur.next=cur.next.next;
+        ListNode cur = dummy.next;
+        ListNode prev = dummy;
+        while(cur!=null){
+            if(set.contains(cur.val)){
+                prev.next=cur.next;
+            }else{
+                prev=cur;
             }
-            else {
-                cur=cur.next;
-            }
+            cur=cur.next;
         }
         return dummy.next;
     }
