dc9cc5f5d44ab71356f60ba11bd9d2b97e80078b
Thissya <125356637+thissya@users.noreply.github.com>
2025-06-21 06:45:17 +0000

    [LeetCode Sync] Runtime - 109 ms (32.01%), Memory - 57.1 MB (84.57%)

--- 0084-largest-rectangle-in-histogram/solution.java
+++ 0084-largest-rectangle-in-histogram/solution.java
@@ -1,19 +1,42 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        Stack<Integer> stack = new Stack<>();
         int n = heights.length;
-        int maxArea = 0;
+        //nsl => Next Smallest Left (check which element is lesser in the left side) eg: 2 left side there is no smaller so -1;
+        int[] nsl= new int[n];
+        int[] nsr= new int[n];
+        Stack<Integer> stk = new Stack<>();
 
-        for(int i = 0; i <= n; i++){
-            int currHeight = (i == n) ? 0 : heights[i];
-            while(!stack.isEmpty() && currHeight < heights[stack.peek()]){
-                int height = heights[stack.pop()];
-                int width = stack.isEmpty() ? i : (i - stack.peek() - 1);
-
-                maxArea = Math.max(maxArea, height * width);
+        for(int i = 0;i < n; i++){
+            while(!stk.isEmpty() && heights[stk.peek()]>=heights[i]){
+                stk.pop();
             }
-            stack.push(i);
+            if(stk.isEmpty()){
+                nsl[i]=-1;
+            }else{
+                nsl[i]=stk.peek();
+            }
+            stk.push(i);
         }
-        return maxArea;
+        stk.clear();
+    
+        for(int i = n-1;i >= 0; i--){
+            while(!stk.isEmpty() && heights[stk.peek()]>=heights[i]){
+                stk.pop();
+            }
+            if(stk.isEmpty()){
+                nsr[i]=n;
+            }else{
+                nsr[i]=stk.peek();
+            }
+            stk.push(i);
+        }
+        int ans =0;
+        for(int i=0;i<n;i++){
+            int width = nsr[i]-nsl[i]-1;
+            int area = width * heights[i];
+            ans = Math.max(ans,area);
+        }
+
+        return ans;
     }
 }
