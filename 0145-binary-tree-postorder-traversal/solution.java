6237ee9ad21f15389f9281c90b406bc521d0532d
Thissya <125356637+thissya@users.noreply.github.com>
2025-06-22 14:50:56 +0000

    [LeetCode Sync] Runtime - 1 ms (7.51%), Memory - 41.7 MB (45.43%)

--- 0145-binary-tree-postorder-traversal/solution.java
+++ 0145-binary-tree-postorder-traversal/solution.java
@@ -14,19 +14,50 @@
  * }
  */
 class Solution {
+    //using Single Stack
     public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> res = new ArrayList<>();
-        postorder(root,res);
+        if(root==null){
+            return res;
+        }
+        Stack<TreeNode> stk1 = new Stack<>();
+        stk1.push(root);
+
+        while(!stk1.isEmpty()){
+            TreeNode p = stk1.pop();
+            res.add(p.val);
+            if(p.left!=null) stk1.add(p.left);
+            if(p.right!=null) stk1.add(p.right);
+        }
+        Collections.reverse(res);
         return res;
     }
 
-    public static void postorder(TreeNode root,List<Integer> res){
-        if(root==null){
-            return ;
-        }
-        
-        postorder(root.left,res);
-        postorder(root.right,res);
-        res.add(root.val);
-    }
 }
+
+/*
+
+// using Two Stack
+
+public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if(root==null){
+            return res;
+        }
+        Stack<TreeNode> stk1 = new Stack<>();
+        Stack<TreeNode> stk2 = new Stack<>();
+        stk1.push(root);
+
+        while(!stk1.isEmpty()){
+            TreeNode p = stk1.pop();
+            stk2.push(p);
+            if(p.left!=null) stk1.add(p.left);
+            if(p.right!=null) stk1.add(p.right);
+        }
+        while(!stk2.isEmpty()){
+            res.add(stk2.pop().val);
+        }
+        return res;
+    }
+
+*/
