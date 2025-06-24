e055d16f48957a445810c22a4fd7cd9ea44babd0
Thissya <125356637+thissya@users.noreply.github.com>
2025-06-24 05:31:56 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.9 MB (88.69%)

--- 0112-path-sum/solution.java
+++ 0112-path-sum/solution.java
@@ -14,35 +14,16 @@
  * }
  */
 class Solution {
-    
     public boolean hasPathSum(TreeNode root, int targetSum) {
-        if(root==null){
-            return false;
-        }
-        int newsum=targetSum-root.val;
+        if(root==null)return false;
+        targetSum-=root.val;
         if(root.left==null && root.right==null){
-            if(newsum==0){
+            if(targetSum==0){
                 return true;
             }else{
                 return false;
             }
         }
-        return hasPathSum(root.left,newsum) || hasPathSum(root.right,newsum);
+        return hasPathSum(root.left,targetSum)|| hasPathSum(root.right,targetSum);
     }
 }
-
-/*
-Approach 1:
-public boolean hasPathSum(TreeNode root, int targetSum) {
-        return recur(root,targetSum,0);    
-    }
-
-    private boolean recur(TreeNode root,int targetSum,int currentSum){
-        if(root==null) return false;
-        System.out.println(currentSum);
-        if(root.left==null && root.right==null && root.val+currentSum == targetSum){
-            return true;
-        }
-        return recur(root.left,targetSum,currentSum+root.val)||recur(root.right,targetSum,currentSum+root.val);
-    }
-*/
