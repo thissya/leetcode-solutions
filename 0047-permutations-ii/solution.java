7274804f1b19c46d54479206e69d8e79b26265ef
Thissya <125356637+thissya@users.noreply.github.com>
2025-06-25 10:15:11 +0000

    [LeetCode Sync] Runtime - 2 ms (72.79%), Memory - 45.2 MB (41.60%)

--- 0047-permutations-ii/solution.java
+++ 0047-permutations-ii/solution.java
@@ -1,27 +1,33 @@
 class Solution {
+    public List<List<Integer>> res = new ArrayList<>();
     public List<List<Integer>> permuteUnique(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        boolean[] flag = new boolean[nums.length];
-        //Arrays.sort(nums); to check duplicates option 2
-        recur(new ArrayList<>(),nums,res,flag);
+        int n = nums.length;
+        Arrays.sort(nums);
+        boolean[] visited = new boolean[n];
+        bt(nums,new ArrayList<>(),visited);
         return res;
     }
-    public static void recur(List<Integer> arr,int[] nums,List<List<Integer>> res,boolean[] flag){
-        if(arr.size()==nums.length){
-            if(!res.contains(arr)){ // to check duplicates option1
-                res.add(new ArrayList<>(arr));
-            }
+
+    public void bt(int[] nums,List<Integer> cur, boolean[] visited){
+        if(cur.size()==nums.length){
+            // if(!res.contains(cur))
+            res.add(new ArrayList<>(cur));
             return ;
         }
-
+        
         for(int i=0;i<nums.length;i++){
-            if(flag[i])continue;
-            //if(i>0 && nums[i]==nums[i-1] && !flag[i-1])continue;  // to Check Duplications option2
-            arr.add(nums[i]);
-            flag[i]=true;
-            recur(arr,nums,res,flag);
-            flag[i]=false;
-            arr.remove(arr.size()-1);
+            if(visited[i]){
+                continue;   
+            }
+            if(i>0 && nums[i]==nums[i-1] && !visited[i-1])continue;
+            cur.add(nums[i]);
+            visited[i]=true;
+            bt(nums,cur,visited);
+            visited[i]=false;
+            cur.remove(cur.size()-1);
+
         }
+
     }
+
 }
