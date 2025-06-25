2cb0b29a5e5c111677938990f1c45df47f9cfbd8
Thissya <125356637+thissya@users.noreply.github.com>
2025-06-25 08:23:02 +0000

    [LeetCode Sync] Runtime - 2 ms (37.67%), Memory - 44.9 MB (10.60%)

--- 0046-permutations/solution.java
+++ 0046-permutations/solution.java
@@ -1,20 +1,22 @@
 class Solution {
-    public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        bt(nums,new ArrayList<>(),res);
-        return res;
-    }
-    public void bt(int[] nums,List<Integer> cur,List<List<Integer>> res){
+    public List<List<Integer>> res = new ArrayList<>();
+    
+    public void bt(int[] nums,List<Integer> cur){
         if(cur.size()==nums.length){
             res.add(new ArrayList<>(cur));
-            return ;
+            return;
         }
-
         for(int i=0;i<nums.length;i++){
-            if(cur.contains(nums[i]))continue;
+            if(cur.contains(nums[i]))
+                continue;
             cur.add(nums[i]);
-            bt(nums,cur,res);
+            bt(nums,cur);
             cur.remove(cur.size()-1);
         }
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+        bt(nums,new ArrayList<>());
+        return res;
     }
 }
