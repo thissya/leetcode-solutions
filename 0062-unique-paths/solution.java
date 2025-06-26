aeaef4bd3daf4f6f0e38def20fbac3a4b36d514e
Thissya <125356637+thissya@users.noreply.github.com>
2025-06-26 04:53:56 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.1 MB (98.11%)

--- 0062-unique-paths/solution.java
+++ 0062-unique-paths/solution.java
@@ -1,18 +1,23 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] arr = new int[m+1][n+1];
-        for(int i=0;i<=m;i++){
-            for(int j=0;j<=n;j++){
-                if(i==0 || j==0){
-                    arr[i][j]=0;
-                }else if(i==1 && j==1){
-                    arr[i][j]=1;
-                }
-                else{
-                    arr[i][j]=arr[i-1][j]+arr[i][j-1];
-                }
-            }
-        }
-        return arr[m][n];
+        int[][] dp = new int[m][n];
+        for(int i=0;i<m;i++)
+            Arrays.fill(dp[i],-1);
+        return recur(0,0,m,n,dp);
     }
+
+    public int recur(int i,int j,int m,int n,int[][] dp){
+        if( i>=m || j>=n){
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        if(i==m-1 && j==n-1){
+            return 1;
+        }
+        dp[i][j]= recur(i+1,j,m,n,dp)+recur(i,j+1,m,n,dp);
+        return dp[i][j];
+    }
+
 }
