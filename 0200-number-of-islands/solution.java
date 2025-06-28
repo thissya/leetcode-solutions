05ad894a5116b7f25071f8c48f6c6362d81a306e
Thissya <125356637+thissya@users.noreply.github.com>
2025-06-28 12:24:03 +0000

    [LeetCode Sync] Runtime - 2 ms (99.81%), Memory - 49.7 MB (46.59%)

--- 0200-number-of-islands/solution.java
+++ 0200-number-of-islands/solution.java
@@ -1,25 +1,27 @@
 class Solution {
     int n,m;
     public int numIslands(char[][] grid) {
-        int ans=0;
-        n=grid.length;
-        m=grid[0].length;
+        n = grid.length;
+        m= grid[0].length;
+        int ans = 0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]=='1'){
                     ans++;
-                    traverse(i,j,grid);
+                    dfs(i,j,grid);
                 }
             }
         }
         return ans;
     }
-    public void traverse(int i,int j,char[][] grid){
-        if(i<0 || j<0 || j>=m || i>=n || grid[i][j]=='0')return ;
+
+    public void dfs(int i,int j,char[][] grid){
+        if(i<0 || j<0 || i>=n || j>=m || grid[i][j]=='0' )return ;
         grid[i][j]='0';
-        traverse(i-1,j,grid);
-        traverse(i,j-1,grid);
-        traverse(i+1,j,grid);
-        traverse(i,j+1,grid);
+        dfs(i+1,j,grid);
+        dfs(i,j+1,grid);
+        dfs(i-1,j,grid);
+        dfs(i,j-1,grid);
     }
+
 }
