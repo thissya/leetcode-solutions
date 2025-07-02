089ce79c421d5f44956ff950c2c3365363e776f5
Thissya <125356637+thissya@users.noreply.github.com>
2025-07-02 16:45:55 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.8 MB (65.09%)

--- 0078-subsets/solution.java
+++ 0078-subsets/solution.java
@@ -1,20 +1,17 @@
 class Solution {
+    public List<List<Integer>> res = new ArrayList<>(); 
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        int n = nums.length;
-        recur(0,res,new ArrayList<>(),nums,n);
+        recur(0,nums,new ArrayList<>());
         return res;
     }
-    
-    public static void recur(int i,List<List<Integer>> res, List<Integer> cur,int[] nums,int n){
-        if(i==n){
-            res.add(cur);
+    public void recur(int i,int[] nums,List<Integer> cur){
+        if(i==nums.length){
+            res.add(new ArrayList<>(cur));
             return;
         }
-        List<Integer> newarr =  new ArrayList<>(cur);
-        newarr.add(nums[i]);
-        recur(i+1,res,newarr,nums,n);
-        recur(i+1,res,cur,nums,n);
+        List<Integer> newcur = new ArrayList<>(cur);
+        recur(i+1,nums,newcur);
+        cur.add(nums[i]);
+        recur(i+1,nums,cur);
     }
-
 }
