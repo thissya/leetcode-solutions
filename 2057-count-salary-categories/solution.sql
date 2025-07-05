f65211a432b6ba23426081b9a52c6eab4edea82d
Thissya <125356637+thissya@users.noreply.github.com>
2025-07-05 18:18:27 +0000

    [LeetCode Sync] Runtime - 1589 ms (41.22%), Memory - 0.0B (100.00%)

--- 2057-count-salary-categories/solution.sql
+++ 2057-count-salary-categories/solution.sql
@@ -1,4 +1,6 @@
 # Write your MySQL query statement below
-select 'Low Salary' as category, sum(income<20000) as accounts_count from Accounts
-union select 'Average Salary' as category, sum(income>=20000 and income<=50000) as accounts_count from Accounts
-union select 'High Salary' as category, sum(income>50000) as accounts_count from Accounts;
+select 'Low Salary' as category , sum(income<20000)as accounts_count from Accounts 
+union 
+    select 'Average Salary' as category , sum(income>=20000 && income<=50000) as accounts_count from Accounts
+union 
+    select 'High Salary' as category ,sum(income>50000) as accounts_count from Accounts order by accounts_count desc;
