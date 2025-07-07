1d6bd6fc965d8027f1ac9aedcd7e1e3cb7262a08
Thissya <125356637+thissya@users.noreply.github.com>
2025-07-07 15:37:14 +0000

    [LeetCode Sync] Runtime - 2 ms (96.80%), Memory - 41.4 MB (96.67%)

--- 0020-valid-parentheses/solution.java
+++ 0020-valid-parentheses/solution.java
@@ -1,36 +1,24 @@
 class Solution {
-    public boolean isValid(String s) 
-    {
-        int top=-1;
-        char[] arr=new char[10000];
-        int n=s.length();
-        for(int i=0;i<n;i++)
-        {
-            char a=s.charAt(i);
-            if(a=='(' || a=='[' ||a=='{')
-            {
-                arr[++top]=a;
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
+            if(c=='{' || c=='[' || c=='('){
+                stack.push(c);
             }
-            else{
-                if(top==-1)
-                {
+            else if(!stack.isEmpty()){
+                if(c=='}' && stack.peek()!='{'){
+                    return false;
+                }else if(c==']' && stack.peek()!='['){
+                    return false;
+                }else if(c==')' && stack.peek()!='('){
                     return false;
                 }
-                char b = arr[top--];
-                if(a==')' && b!='(')
-                {
-                    return false;
-                } 
-                if(a==']' && b!='[')
-                {
-                    return false;
-                }
-                if(a=='}' && b!='{')
-                {
-                    return false;
-                }
+                stack.pop();
+            }else{
+                return false;
             }
         }
-        return top==-1;
+        return stack.isEmpty();
     }
 }
